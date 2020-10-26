690ea69c304c5839b02e4c085d33d0c32c284143
Tadeo Grach <tade.g.17@gmail.com>
2020-10-26 20:06:44 +0000

    fixing

--- _printf.c
+++ _printf.c
@@ -22,7 +22,7 @@ int _printf(const char *format, ...)
 	va_start(list, format);
 	if (format == NULL)
 	{
-		write(1, "(null)", 7);
+		/*write(1, "(null)", 7);*/
 		return (-1);
 	}
 	for (i = 0; format[i] != '\0'; i++)
