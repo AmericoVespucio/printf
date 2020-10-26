7815bf7013f99aa7610056e9c66555a465b8c246
AmericoVespucio <70416497+AmericoVespucio@users.noreply.github.com>
2020-10-26 00:03:43 -0300

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -1,15 +1,41 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* ALIAS */
+typedef const char *format;
+/* END */
+
+/* STRUCTURE */
 /**
- * struct tipo - Struct tipo
- *
- * @c: the character
- * @f: The function associated
+ * struct tFormat - Checks the formats.
+ * @id: Identifier.
+ * @f: Functions.
+ * struct tFormat - Checks the formats.
  */
-typedef struct print_arguments
+typedef struct tFormat
 {
-	char *c;
-	void (*f)();
-} pa;
-int _printf(const char *format, ...);
-#endif
+	char *id;
+	int (*f)();
+} funcs;
+/* END */
+
+/* PROTOTYPES */
+int _printf(format, ...);
+int print_main(int);
+int print_char(va_list);
+int print_percent(void);
+int print_str(va_list);
+int print_integer(va_list);
+int print_in_bases(va_list, int, int);
+int print_rev(va_list);
+int rot13(va_list);
+void _reverse(char *);
+int checkBase(char);
+int checker(format, int, struct tFormat fmtF[], va_list);
+/* END */
+
+#endif /*_PRINTF_*/
